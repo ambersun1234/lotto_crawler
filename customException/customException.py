4d93682890ef55d08962c273bb73fdee67ebfd05
ambersun1234 <ambersun1019.shawn@gmail.com>
2021-02-21 00:35:03 +0800

    Refactor exception and constant

--- customException/customException.py
+++ customException/customException.py
@@ -1,13 +1,16 @@
 class lotteryEmptyError(Exception):
 	def __init__(self):
-		super().__init__("lottery empty: no data found")
+		super().__init__("no data found")
+
+class lotteryPageError(Exception):
+	def __init__(self):
+		super().__init__("error getting page")
+
+class lotteryValidationFailed(Exception):
+	def __init__(self):
+		super().__init__("cannot get validation argument")
 
 class serverError(Exception):
 	def __init__(self, code):
 		super().__init__("HTTP status code: {}".format(code))
-
-class lotteryValidationFailed(Exception):
-	def __init__(self):
-		super().__init__("lottery validation failed: cannot get validation argument")
-
 
---
+++ lotteryConstant/lotteryConstant.py
@@ -0,0 +1,25 @@
+class lotteryConstant:
+	# 威力彩, 大樂透, 今彩539,
+	# 雙贏彩, 三星彩, 四星彩,
+	# 38樂合彩, 49樂合彩, 39樂合彩
+	lottery_type = [
+		"lotto/superlotto638", "lotto/Lotto649", "lotto/DailyCash", 
+		"Lotto1224", "Lotto/3D", "Lotto/4D", 
+		"lotto/38M6", "Lotto/49M6", "Lotto/39M5"
+	]
+	lottery_header = [
+		"SuperLotto638", "Lotto649", "D539",
+		"Lotto1224", "L3D", "L4D",
+		"M638", "M649", "M539"
+	]
+	lottery_control = {
+		"SuperLotto638": "1", "Lotto649": "", "D539": "1",
+		"Lotto1224" : "", "L3D": "1", "L4D": "1",
+		"M638": "1", "M649": "1", "M539": "1"
+	}
+	lottery_dropdown = [
+		1, 2, 3,
+		4, 6, 7,
+		8, 9, 10
+	]
+
